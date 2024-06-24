be85e64db500bf6548f4bba02ed8160cccf82d3d
bluesbox1 <40844619+bluesbox1@users.noreply.github.com>
2024-06-24 21:12:40 +1200

    clean up file path

--- generate-schema.py
+++ generate-schema.py
@@ -2,7 +2,7 @@ import json
 from genson import SchemaBuilder
 
 builder = SchemaBuilder()
-with open('./source.json', 'r') as f:
+with open('source.json', 'r') as f:
     datastore = json.load(f)
     builder.add_object(datastore )
 
@@ -10,6 +10,6 @@ builder.to_schema()
 json_schema = builder.to_json(indent=2)
 print(json_schema)
 
-f = open("./schema.json", "w")
+f = open("schema.json", "w")
 f.write(json_schema)
 f.close()
